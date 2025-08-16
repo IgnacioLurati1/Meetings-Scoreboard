78d672cc7abf803231385b8c189f9d4d6bf57301
IgnacioLurati1 <ilurati@frro.utn.edu.ar>
2025-08-16 20:42:52 -0300

    Adapted to render deploy

--- src/app.js
+++ src/app.js
@@ -15,6 +15,7 @@ app.use('/api/scoreboard', scoreboardRouter);
 app.use('/api/login', loginRouter);
 
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
